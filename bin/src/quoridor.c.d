fe388e87249ee1105b28085df96fd82497a79262
Alexandros <alexandrosmi17@gmail.com>
2024-06-24 16:34:13 +0300

    minor updates due to vector module

--- bin/src/quoridor.c.d
+++
@@ -1,8 +0,0 @@
-bin/./src/quoridor.c.o: src/quoridor.c include/quoridor_functions.h \
- include/types/quoridor_types.h include/types/common_types.h \
- include/modules/vector.h include/helpers/utils.h
-include/quoridor_functions.h:
-include/types/quoridor_types.h:
-include/types/common_types.h:
-include/modules/vector.h:
-include/helpers/utils.h:
